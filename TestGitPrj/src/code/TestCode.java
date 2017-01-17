24c75e2eb6dc2279648ea209b18745560b76c2f0
Dipankar Saha <sahadipankarind83@gmail.com>
2017-01-16 21:04:29 -0600

    Removed a sysout to create a difference

--- TestGitPrj/src/code/TestCode.java
+++ TestGitPrj/src/code/TestCode.java
@@ -4,8 +4,7 @@ public class TestCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello test code 1");    
-		System.out.println("Hello test code 2");  
+		System.out.println("Hello test code");      
 	}
 
 }
